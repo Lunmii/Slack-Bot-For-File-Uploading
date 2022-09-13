e8e8422fe5b47775ba6ed1902e38b39153fa7044
Alo Oluwapelumi David <pelumidavid123456@gmail.com>
2022-09-13 17:50:50 +0100

    commit

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-funnc() {
+import (
+	"fmt"
+	"github.com/slack-go/slack"
+	"os"
+)
+
+func main() {
 	os.Setenv("SLACK_BOT_TOKEN", "")
 	os.Setenv("CHANNEL_ID", "")
-	api := slack.
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{""}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		File, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL:%\n", file.Name file.URL)
+	}
 }
